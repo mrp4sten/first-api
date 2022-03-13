1d9bdeb12d3a289ffa62ef70b7684fca21cfb2d2
mrp4sten <mauricio.pasten.martinez@gmail.com>
2022-03-12 21:10:36 -0600

    creating register route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,16 +1,34 @@
-const express = require('express');
-const Users = require('../models/Users')
+const express = require("express");
+const crypto = require("crypto");
+const Users = require("../models/Users");
 const router = express.Router();
 
-
-router.post('/register', (req, res) => {
-    Users.create(req.body)
-        .then(x => res.status(201).send(x));
+router.post("/register", (req, res) => {
+  const { email, password } = req.body;
+  crypto.randomBytes(16, (err, salt) => {
+    const newSalt = salt.toString("base64");
+    crypto.pbkdf2(password, newSalt, 1000, 64, "sha1", (err, key) => {
+      const encryptedPassword = key.toString("base64");
+      Users.findOne({ email })
+        .exec()
+        .then((user) => {
+          if (user) {
+            return res.send("User already exists");
+          }
+          Users.create({
+            email,
+            password: encryptedPassword,
+            salt: newSalt,
+          }).then(() => {
+            res.send("User created successfully");
+          });
+        });
+    });
+  });
 });
 
-router.post('/login', (req, res) => {
-    Users.create(req.body)
-        .then(x => res.status(201).send(x));
+router.post("/login", (req, res) => {
+  Users.create(req.body).then((x) => res.status(201).send(x));
 });
 
 module.exports = router;
